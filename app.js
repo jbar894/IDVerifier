bb9079b2db657f1d0846429d883d9a5e2959aa7d
jbar894 <j.barwellsmith@gmail.com>
2018-09-09 18:56:05 +1200

    added label detection and made code nicer

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get('/', function(req, res){
 app.post('/upload', function(req, res){
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
+	
+	var keywords;
+
+	axios.post("https://vision.googleapis.com/v1p3beta1/images:annotate?key=" + config.key, {
+		"requests": [
+			{
+			"image": {
+					"content": req.files.image.data.toString('base64')
+			},
+			"features": [
+				{
+				"type": "LABEL_DETECTION"
+				}
+			]
+			}
+		]
+	}).then(function(response) {
+		keywords = response.data.responses[0].labelAnnotations.map(item =>{
+			return{
+				description: item.description,
+				score: item.score
+			}
+		});
+	});
 
     axios.post("https://vision.googleapis.com/v1p3beta1/images:annotate?key=" + config.key, {
         "requests": [
@@ -30,21 +54,39 @@ app.post('/upload', function(req, res){
             ]
           }
         ]
-      }).then(function(response) {
+    }).then(function(response) {
         console.log(response.data.responses[0].textAnnotations[0].description.match(/\d\w?\. ([a-zA-Z0-9-]+)?/gm));
-        var id = {};
-        response.data.responses[0].textAnnotations[0].description.match(/\d\w?\. ([a-zA-Z0-9-]+)?/gm).forEach(element => {
-            var line = element.split('. ');
-            id[line[0]]= line[1];
-        });
-            console.log(id);
-            console.log(id["4b"]);
-          console.log(response.data.responses[0].textAnnotations[0].description);
-          // \d\w?\. ([a-zA-Z0-9-]+)?
-          //console.log(typeof response.data);
-          //console.log(JSON.stringify(response.data));
-          res.send("<pre>" + JSON.stringify(id,null,4) + "</pre>");
-      });
+
+        var fullText = response.data.responses[0].textAnnotations[0].description;
+
+        if(fullText.includes("DRIVER LICENCE") && fullText.includes("NEW ZEALAND")){
+            var id = {};
+            fullText.match(/\d\w?\. ([a-zA-Z0-9-]+)?/gm).forEach(element => {
+            	var line = element.split('. ');
+            	id[line[0]]= line[1];
+			});
+			id["keywords"] = keywords;
+        	console.log(id);
+        	console.log(id["4b"]);
+        	console.log(fullText);
+			// \d\w?\. ([a-zA-Z0-9-]+)?
+			//console.log(typeof response.data);
+			//console.log(JSON.stringify(response.data));
+			res.send("<pre>" + JSON.stringify(id,null,4) + "</pre>");
+		} else if(fullText.includes("PASSPORT") && fullText.includes("NEW ZEALAND")) {
+			var id = {};
+			id["keywords"] = keywords;
+			var passNumIndex = response.data.responses[0].textAnnotations.findIndex(function(item){
+				return item.description=="Uruwhenua" ;
+			});
+			id["passportNumber"] = response.data.responses[0].textAnnotations[passNumIndex+1].description;
+			res.send("<pre>" + JSON.stringify(id,null,4) + "</pre>");
+			//res.send("<pre>" + JSON.stringify(response.data.responses[0].textAnnotations,null,4) + "</pre>");
+		} else {
+			res.send("Document not supported");
+		}
+			
+		});
 
     //console.log(req.files.image.data.toString('base64'));
     //console.log(base64Encode(req.files.image.data));
